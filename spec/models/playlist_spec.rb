e625424d2981df6ae9d03035956f206c2d92bc42
tonwanchai <grand38326@gmail.com>
2021-07-02 08:43:46 +0700

    edit test playlist

--- spec/models/playlist_spec.rb
+++ spec/models/playlist_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Playlist, type: :model do
   it { should have_many(:playlist_musics) }
-
   it { should validate_presence_of(:name) }
+  it { should have_many(:musics).through(:playlist_musics) }
 end
