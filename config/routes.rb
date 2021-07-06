f09b2ebacbbf7247368cdd24450debf2edb7b6df
tonwanchai <grand38326@gmail.com>
2021-07-06 17:12:50 +0700

    edit routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 Rails.application.routes.draw do
   
   resources :musics
-  resources :playlists
+  resources :playlists do
+    member do
+      get :get_all_music
+      patch :add_music_to_playlist
+      patch :delete_music_from_playlist
+    end
+  end
+
+  
   resources :playlists_musics
   post "playlists_musics/new"  => "playlists_musics#create"
-  namespace :api, defaults: { format: 'json' } do
-    resources :musics, only: [:show]
-  end
+  #namespace :api, defaults: { format: 'json' } do
+  #  resources :musics, only: [:show]
+  #end
   root to: 'musics#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
